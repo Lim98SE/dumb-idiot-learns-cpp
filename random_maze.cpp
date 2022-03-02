7f69f5f307336042f013b39d2e6b7c9e119f3399
Lim95 <73658212+Lim98SE@users.noreply.github.com>
2022-03-02 11:02:39 -0600

    made the randomness better?

--- random_maze.cpp
+++ random_maze.cpp
@@ -5,12 +5,15 @@ using namespace std;
 
 int main() {
     int num; // initalise number
+    int ticks; // edited a lil bit
     while (true){ // repeat forever
+        srand(time(NULL)+ticks);
         num=rand()%2; // set num to 0 or 1
         if (num==1){ // if num is 1
             cout << "/"; // print /
         } else { // if num is 0
             cout << "\\"; // print the backslash
         }
+        ticks++;
     }
 }
